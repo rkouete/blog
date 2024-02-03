5294c6bbb800d378454eea7743b9a6d390ff5abb
rkouete <gausskt35@gmail.com>
2024-02-03 14:09:30 +0100

    some updates

--- components/layout/main-footer.js
+++ components/layout/main-footer.js
@@ -1,8 +1,8 @@
-import { useContext } from 'react';
-import classes from './main-footer.module.css';
-import { useRef } from 'react';
-import ThemeContext from '../../store/theme-context';
-import NotificationContext from '../../store/notification-context';
+import { useContext } from "react";
+import classes from "./main-footer.module.css";
+import { useRef } from "react";
+import ThemeContext from "../../store/theme-context";
+import NotificationContext from "../../store/notification-context";
 
 function MainFooter() {
   const emailInputRef = useRef();
@@ -15,50 +15,54 @@ function MainFooter() {
     const enteredEmail = emailInputRef.current.value;
 
     notificationCtx.showNotification({
-      title: 'Please wait !',
-      message: 'The action is loading.',
-      status: 'pending'
+      title: "Please wait !",
+      message: "The action is loading.",
+      status: "pending",
     });
 
-    fetch('/api/newsletter', {
-      method: 'POST',
+    fetch("/api/newsletter", {
+      method: "POST",
       body: JSON.stringify({ email: enteredEmail }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     }).then((response) => {
-      console.log(response.message)
       notificationCtx.showNotification({
-        title: 'Success !',
-        message: 'The Email address send successfully.',
-        status: 'success'
+        title: "Success !",
+        message: "The Email address send successfully.",
+        status: "success",
       });
-    })
-  }
+    });
+  };
 
   return (
-    <div className={themeCtx.theme === 'DARK' ? classes.darkFooter : classes.lightFooter}>
-      <h3 className={classes.titleNewsletter}>Interesting Stories | Updates | Guides</h3>
+    <div
+      className={
+        themeCtx.theme === "DARK" ? classes.darkFooter : classes.lightFooter
+      }
+    >
+      <h3 className={classes.titleNewsletter}>
+        Interesting Stories | Updates | Guides
+      </h3>
       <p className={classes.describe}>
-        Subscribe to learn about new technology and updated. join over 5000+ 
+        Subscribe to learn about new technology and updated. join over 5000+
         members community to stay up to date with latest news.
       </p>
       <form className={classes.form} onSubmit={registrationHandle}>
         <input
           className={classes.input}
-          type='email'
-          id='email'
-          placeholder='Enter your Email'
+          type="email"
+          id="email"
+          placeholder="Enter your Email"
           ref={emailInputRef}
         />
         <button className={classes.btn}>Suscribe</button>
       </form>
       <div className={classes.bottom}>
-        <span>&copy;2023 Kevin, All rights reserved.</span>
-        <div>Made in &hearts; by <a href='#'>Kevin</a></div>
+        <span>&copy;2023 Rodrigue KOUETE, All rights reserved.</span>
       </div>
     </div>
-  )
+  );
 }
 
-export default MainFooter
+export default MainFooter;
