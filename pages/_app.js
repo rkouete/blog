32eb81e3adfab73ae9d48f5c27bc8b41c49db30e
rkouete <gausskt35@gmail.com>
2024-02-03 15:24:07 +0100

    delete next.config file

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,34 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import Layout from '../components/layout/layout';
+import Layout from "../components/layout/layout";
 import "../styles/global.css";
-import { Roboto } from 'next/font/google';
-import { ThemeContextProvider } from '../store/theme-context';
-import { NotificationContextProvider } from '../store/notification-context';
+import { Roboto } from "next/font/google";
+import { ThemeContextProvider } from "../store/theme-context";
+import { NotificationContextProvider } from "../store/notification-context";
 
 const roboto = Roboto({
-    weight: '400',
-    subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
 function App({ Component, pageProps }) {
-    return (
-        <NotificationContextProvider>
-            <ThemeContextProvider>
-                <Layout>
-                    <Head>
-                        <meta name='viewport' content='width=device-width, initial-scale=1' />
-                    </Head>
-                    <main className={roboto.className}>
-                        <Component {...pageProps} />
-                    </main>
-                </Layout>
-            </ThemeContextProvider>
-        </NotificationContextProvider>
-    );
+  return (
+    <NotificationContextProvider>
+      <ThemeContextProvider>
+        <Layout>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+          </Head>
+          <main className={roboto.className}>
+            <Component {...pageProps} />
+          </main>
+        </Layout>
+      </ThemeContextProvider>
+    </NotificationContextProvider>
+  );
 }
 
-export default App
+export default App;
